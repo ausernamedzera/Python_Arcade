2866df3395a5d6e44b0ebf94c6b4355a1a15c7e8
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-20 15:31:11 +0300

    collision_mechanic_v1

--- main.py
+++ main.py
@@ -63,7 +63,8 @@ class GameWindow(arcade.Window):
             enemy[1] -= 3
             self.enemies = [e for e in self.enemies if e[1] > 0]
 
-        for bullet in self.bullets:
+        for bullet in self.bullets[:]:
+            for enemy in self.enemies[:]:
 
 
 def main():
